9a2842138facdde2457b8a25b06bbcc83d7bbb40
wahajalfraih <jojo911alfraih@gmail.com>
2025-08-20 23:41:45 +0300

    GOT UPDATED

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = prompt("enter your name");
+let age = prompt("enter your age");
+const city = "kuwait";
+alert("Hello " + name + " , your age is " + age);
+let outputColor = "color: green;";
+
+console.log("%cName: " + name, outputColor);
+console.log("%cAge: " + age, outputColor);
+console.log("%cCity: " + city, outputColor);
